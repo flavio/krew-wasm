d5782d554f424cad7ef6090474d2b447de26b7e6
Flavio Castelli <fcastelli@suse.com>
2022-04-12 11:17:25 +0200

    Small fix when invoking the wasm plugin in "native" modified
    Ensure the argv[0] contains the right name of the plugin

--- src/main.rs
+++ src/main.rs
@@ -86,15 +86,20 @@ fn run_native(cli: cli::Native) {
     match cli.command {
         NativeCommands::Run { module, wasm_args } => {
             let wasm_module_path = Path::new(module.as_str());
+
+            let wasm_filename = wasm_module_path.file_name().unwrap().to_string_lossy();
+            let plugin_name = wasm_filename
+                .strip_suffix(".wasm")
+                .map(|s| s.to_string())
+                .unwrap_or_else(|| wasm_filename.to_string());
+            let kubectl_plugin_name = if plugin_name.starts_with("kubectl-") {
+                plugin_name
+            } else {
+                format!("kubectl-{}", plugin_name)
+            };
+
             let mut wasm_args = wasm_args.clone();
-            wasm_args.insert(
-                0,
-                wasm_module_path
-                    .file_name()
-                    .unwrap()
-                    .to_string_lossy()
-                    .to_string(),
-            );
+            wasm_args.insert(0, kubectl_plugin_name);
             let wasi_args = wasm_host::WasiArgs::UserProvided(wasm_args);
 
             match wasm_host::run_plugin(wasm_module_path.to_path_buf(), &wasi_args) {
